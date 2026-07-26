6f9b1ed3cfe443a6ad24f2132e0ff70e458cf9a6
Prathick Dhanes R <selftrust.6886@gmail.com>
2026-07-27 01:55:06 +0530

    Add structured planning types

---
+++ app/features/planning/types.ts
@@ -0,0 +1,8 @@
+import type { ConstraintSource, Daypart, EnergyLevel, GeneratedTask, PlanResult, Subtask } from "../../types"
+
+export type PlannedTask = GeneratedTask
+export type PlannedSubtask = Subtask
+export type PlanningDaypart = Daypart
+export type PlanningEnergy = EnergyLevel
+export type TimingConstraintSource = ConstraintSource
+export type StructuredPlan = PlanResult
